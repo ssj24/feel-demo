e6fb7fe70a65f869d0b6abec9c870b4aeeff9b1c
ssj24 <sujee0824@gmail.com>
2022-11-09 23:42:10 +0900

    add datetime

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { DatetimeChangeEventDetail, DatetimeCustomEvent, OverlayEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  @ViewChild(IonModal) modal:IonModal;
+  message = 'modal example';
+  name: string;
+  dateChanged(e: Event) {
+    const ev = e as CustomEvent<DatetimeCustomEvent>;
+    const detail = ev.detail as DatetimeChangeEventDetail;
+    console.log(e);
+    console.log(detail.value);
+  }
+  dateClick(e: Event) {
+    console.log(e);
+  }
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}`;
+    }
+  }
   constructor() {}
 
 }
